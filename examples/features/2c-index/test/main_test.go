4b80160574fc1a5e02625f90bd3f7cb8ceda2bac
VJ Patel <vj@vjpatel.me>
2024-04-19 22:53:05 +0100

    add 2c-index feature

---
+++ examples/features/2c-index/test/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/go-rod/rod"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestApp(t *testing.T) {
+	fs := http.FileServer(http.Dir("../build"))
+	s := httptest.NewServer(fs)
+	defer s.Close()
+
+	page := rod.New().MustConnect().
+		Timeout(10 * time.Second).
+		Logger(rod.DefaultLogger).
+		MustPage(s.URL)
+	defer page.MustClose()
+	page.MustWaitStable()
+
+	appEl := page.MustElement("#app")
+
+	appHTML := appEl.MustHTML()
+	assert.Equal(t,
+		`<div id="app"><ul><vigor-index><li><a target="_blank" href="https://www.youtube.com/watch?v=J---aiyznGQ">1: Keyboard Cat</a></li><li><a target="_blank" href="https://www.youtube.com/watch?v=z_AbfPXTKms">2: Maru</a></li><li><a target="_blank" href="https://www.youtube.com/watch?v=OUtn3pvWmpg">3: Henri The Existential Cat</a></li></vigor-index></ul></div>`,
+		appHTML,
+	)
+}
